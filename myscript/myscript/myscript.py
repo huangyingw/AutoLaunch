697db7c6eef40ee34376a3f49691a99e5c74eb78
Ying Huang <yhuang@imvu.com>
2019-01-12 21:19:19 -0800

    n

--- myscript/myscript/myscript.py
+++ myscript/myscript/myscript.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3.7
 
 import iterm2
+import objc
+from AppKit import *
 # To install packages from PyPI, use this command, changing package_name to the package you
 # wish to install:
 #   "/Users/huangyingw/Library/ApplicationSupport/iTerm2/Scripts/myscript/iterm2env/versions/3.7.1/bin/pip3" install package_name
+
 
 async def main(connection):
     app = await iterm2.async_get_app(connection)
@@ -19,6 +22,10 @@ async def main(connection):
             window = await iterm2.Window.async_create(connection)
             await window.async_set_fullscreen(False)
             await window.async_set_tabs([tab_to_move])
+
+            screenFrame = NSScreen.screens()[0].frame()
+            new_window_frame = iterm2.Frame(iterm2.Point(screenFrame.origin.x, screenFrame.origin.y), iterm2.Size(screenFrame.size.width, screenFrame.size.height))
+            await window.async_set_frame(new_window_frame)
         else:
             window = app.terminal_windows[j]
             await window.async_set_tabs(window.tabs + [tab_to_move])
